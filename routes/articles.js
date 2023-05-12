8e89417d971c70fb27ee6ba8ccad3301c906bd7b
ELMOUADDIBE <elmouaddibezaid@gmail.com>
2023-05-12 18:33:35 +0100

    Ajout des routes/.js et des fichiers de test

---
+++ routes/articles.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  // 
+});
+
+router.get('/:id', (req, res) => {
+  // 
+});
+
+router.post('/', (req, res) => {
+  //
+});
+
+router.patch('/', (req, res) => {
+  //
+});
+
+router.delete('/:id', (req, res) => {
+  //
+});
+
+module.exports = router;
