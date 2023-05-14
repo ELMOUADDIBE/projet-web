9734e3b2f84b54388b8edeb9260a8759b85bae74
ELMOUADDIBE <elmouaddibezaid@gmail.com>
2023-05-14 21:55:50 +0100

    Installation Faker / Modification de routers .js

--- routes/articles.js
+++ routes/articles.js
@@ -1,24 +1,40 @@
 const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // 
+router.get('/', async (req, res) => {
+    const { take, skip } = req.query;
+    const articles = await prisma.article.findMany({ take: +take, skip: +skip });
+    res.json(articles);
 });
 
-router.get('/:id', (req, res) => {
-  // 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const article = await prisma.article.findUnique({ where: { id: +id } });
+    res.json(article);
 });
 
-router.post('/', (req, res) => {
-  //
+router.post('/', async (req, res) => {
+    const newArticle = req.body;
+    const article = await prisma.article.create({ data: newArticle });
+    res.json(article);
 });
 
-router.patch('/', (req, res) => {
-  //
+router.patch('/', async (req, res) => {
+    const updatedArticle = req.body;
+    const article = await prisma.article.update({ 
+        where: { id: updatedArticle.id },
+        data: updatedArticle
+    });
+    res.json(article);
 });
 
-router.delete('/:id', (req, res) => {
-  //
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    await prisma.article.delete({ where: { id: +id } });
+    res.json({ message: `Article with id ${id} deleted.` });
 });
 
 module.exports = router;
